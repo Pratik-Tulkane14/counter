9abbdc122ab4dfd7b21cb652f20e6cb84506e495
Pratik Tulkane <pratiktulkane12345@gmail.com>
2024-09-16 22:16:50 +0530

    fix: ogic of time in minutes

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ function App() {
   };
   const formatTime = (seconds) => {
     const timeInMintes = Math.floor(seconds / 60);
-    
+
     const timeInSeconds = seconds % 60;
     const paddedTime = String(timeInSeconds).padStart(2, "0");
-    return `${timeInMintes}:${paddedTime}`;
+    return `${timeInMintes}:${paddedTime} ${timeInMintes > 0 ? "minute" : ""}`;
   };
   const handleToggle = () => {
     setIsCounterStart(!isCounterStart);
